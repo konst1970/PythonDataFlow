96046f2d271868f611275cd5d264ff69324b7f18
Vlad Medvid <57641251+Drakonchik2@users.noreply.github.com>
2021-03-22 15:33:14 +0200

    Create Component.py

---
+++ Component.py
@@ -0,0 +1,11 @@
+class Component:
+    def __init__(self):
+        self.inputs = [] 
+        self.outputs = [] 
+ 
+    def process(self):
+        pass
+
+    def validate(self):
+        pass
+ 
