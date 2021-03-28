7d526d9142a536a6fc8ff9e9d9e110ca6853c701
Vlad Medvid <57641251+Drakonchik2@users.noreply.github.com>
2021-03-29 01:59:57 +0300

    Update Component.py

--- Component.py
+++ Component.py
@@ -1,7 +1,10 @@
 class Component:
-    def __init__(self):
+    def __init__(self, inputs=[]):
         self.inputs = [] 
-        self.outputs = [] 
+        self.outputs = []
+        if inputs:
+            for i in inputs:
+                self.inputs.append(i) 
  
     def process(self):
         pass
